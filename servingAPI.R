87e4a95311570d28767bd72e74ea5b781b8dc6f9
thezon <thezon@live.com>
2020-01-26 21:36:29 -0500

    base

---
+++ servingAPI.R
@@ -0,0 +1,29 @@
+library(plumber)
+library(logging)
+
+basicConfig()
+
+intelligentSecurity <- function (...){
+  logdebug(paste("hit: ",...))
+}
+
+
+#* @apiTitle Simple API
+
+#* Echo provided text
+#* @param text The text to be echoed in the response
+#* @get /mockEndpoint
+function(text = "No Parameter") {
+  intelligentSecurity(text)
+  
+  list(
+    message_echo = paste("The text is:", text)
+  )
+}
+
+
+
+
+# how to run ~ https://www.rplumber.io/
+#plumb("servingAPI.R")$run(port = "4444")
+
